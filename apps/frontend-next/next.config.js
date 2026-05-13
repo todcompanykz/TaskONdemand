fb6226fc1fe9ff92c071f9f6c9c282e45c60c0ec
todcompanykz <didar@todcompanykz.com>
2026-05-14 00:49:51 +0600

    Disable next-pwa for Vercel deployment

--- apps/frontend-next/next.config.js
+++ apps/frontend-next/next.config.js
@@ -1,24 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: true, // Temporarily disable PWA to avoid cache issues
-  buildExcludes: [/app-build-manifest\.json$/],
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'offlineCache',
-        expiration: {
-          maxEntries: 200,
-        },
-      },
-    },
-  ],
-})
-
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
@@ -26,4 +6,4 @@ const nextConfig = {
   },
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
